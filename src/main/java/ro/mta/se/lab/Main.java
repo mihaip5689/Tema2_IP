fb8431111cff75002f112353bee695882253df29
Mihai <mihai.paun@mta.ro>
2021-01-12 22:07:37 +0200

    Implementare interfata 1.0
    Am creat o interfata minimala si fisierul cu locatii

--- src/main/java/ro/mta/se/lab/Main.java
+++ src/main/java/ro/mta/se/lab/Main.java
@@ -1,4 +1,29 @@
 package ro.mta.se.lab;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends Application{
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        FXMLLoader loader = new FXMLLoader();
+
+        try {
+            loader.setLocation(this.getClass().getResource("/view/MeteoView.fxml"));
+            primaryStage.setScene(new Scene(loader.load()));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
