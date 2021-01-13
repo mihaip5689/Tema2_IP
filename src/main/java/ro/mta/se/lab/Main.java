5b39f5ab661ad228ec9c71e1e4bf2ddc24c140d9
Mihai <mihai.paun@mta.ro>
2021-01-13 14:02:23 +0200

    Incarcare locatii din fisierul json

--- src/main/java/ro/mta/se/lab/Main.java
+++ src/main/java/ro/mta/se/lab/Main.java
@@ -4,10 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
-import java.net.URL;
+import ro.mta.se.lab.controller.MeteoController;
 
-public class Main extends Application{
+import java.io.IOException;
+
+public class Main extends Application {
 
     public static void main(String[] args)
     {
@@ -20,6 +21,7 @@ public class Main extends Application{
 
         try {
             loader.setLocation(this.getClass().getResource("/view/MeteoView.fxml"));
+            loader.setController(new MeteoController());
             primaryStage.setScene(new Scene(loader.load()));
             primaryStage.show();
         } catch (IOException e) {
--- src/main/java/ro/mta/se/lab/controller/MeteoController.java
+++ src/main/java/ro/mta/se/lab/controller/MeteoController.java
@@ -1,4 +1,86 @@
 package ro.mta.se.lab.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import ro.mta.se.lab.model.MeteoModel;
+import org.json.simple.*;
+
+import javafx.event.ActionEvent;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MeteoController {
+
+    private MeteoModel meteoModel;
+    private Map<String,String> locations = new HashMap<>();
+
+    @FXML
+    private ChoiceBox tari;
+
+    @FXML
+    private ChoiceBox orase;
+
+    @FXML
+    private void initialize(){
+        JSONParser parser = new JSONParser();
+        ObservableList<String> t = FXCollections.observableArrayList();
+
+        try {
+            JSONArray loc = (JSONArray) parser.parse(
+                    new FileReader("./src/main/resources/locations.json"));
+
+            for(Object l : loc){
+                JSONObject location = (JSONObject) l;
+
+                String ID = (String) location.get("ID");
+                String nm = (String) location.get("nm");
+                String lat = (String) location.get("lat");
+                String lon = (String) location.get("lon");
+                String countryCode = (String) location.get("countryCode");
+
+                if(!locations.containsValue(countryCode))
+                    t.add(countryCode);
+
+                locations.put(nm,countryCode);
+            }
+
+            tari.setItems(t);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void LoadCities(ActionEvent e){
+        ObservableList<String> o = FXCollections.observableArrayList();
+
+        if(tari.getValue()!=null){
+            for(Map.Entry<String,String> entry : locations.entrySet()){
+                if(tari.getValue().toString().equals(entry.getValue())) {
+                    o.add(entry.getKey());
+                }
+            }
+        }
+
+        orase.getItems().clear();
+        orase.setItems(o);
+    }
+
+    public MeteoController() {
+
+    }
+
 }
