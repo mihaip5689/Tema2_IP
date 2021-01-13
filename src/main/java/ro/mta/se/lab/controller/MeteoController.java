f9cee4d9f041e2226f65ba4303b105fe44b56e2d
Mihai <mihai.paun@mta.ro>
2021-01-13 15:57:37 +0200

    Implementare model + controller
    Am implementat clasa MeteoModel si am adaugat cateva functionalitati la clasa MeteoController: legatura cu modelul, log-ul cautarilor in fisier, legatura cu view-ul

--- src/main/java/ro/mta/se/lab/controller/MeteoController.java
+++ src/main/java/ro/mta/se/lab/controller/MeteoController.java
@@ -3,7 +3,9 @@ package ro.mta.se.lab.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import ro.mta.se.lab.model.MeteoModel;
@@ -11,22 +13,34 @@ import org.json.simple.*;
 
 import javafx.event.ActionEvent;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MeteoController {
 
     private MeteoModel meteoModel;
-    private Map<String,String> locations = new HashMap<>();
+    private Map<String,String> locations;
 
     @FXML
     private ChoiceBox tari;
 
     @FXML
     private ChoiceBox orase;
+
+    @FXML
+    private Button cauta;
+
+    @FXML
+    private Label temperature;
+
+    @FXML
+    private Label wind;
+
+    @FXML
+    private Label humidity;
 
     @FXML
     private void initialize(){
@@ -36,6 +50,11 @@ public class MeteoController {
         try {
             JSONArray loc = (JSONArray) parser.parse(
                     new FileReader("./src/main/resources/locations.json"));
+
+            File f = new File("./src/main/resources/log.txt");
+
+            if(!f.exists())
+                f.createNewFile();
 
             for(Object l : loc){
                 JSONObject location = (JSONObject) l;
@@ -53,6 +72,10 @@ public class MeteoController {
             }
 
             tari.setItems(t);
+
+            temperature.setVisible(false);
+            wind.setVisible(false);
+            humidity.setVisible(false);
 
         } catch (ParseException e) {
             e.printStackTrace();
@@ -79,8 +102,40 @@ public class MeteoController {
         orase.setItems(o);
     }
 
-    public MeteoController() {
+    @FXML
+    public void LoadWeatherData(ActionEvent e){
+        meteoModel = new MeteoModel(orase.getValue().toString());
 
+        this.temperature.setText(meteoModel.getTemperature()+"\u00B0C");
+        this.wind.setText(meteoModel.getWind()+"km/h");
+        this.humidity.setText(meteoModel.getHumidity()+"%");
+
+        wind.setVisible(true);
+        humidity.setVisible(true);
+        temperature.setVisible(true);
+
+        LogSearch(orase.getValue().toString(), tari.getValue().toString(), temperature.getText(), wind.getText(), humidity.getText());
+    }
+
+    private void LogSearch(String city, String country, String temp, String wind, String hum){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        try {
+            FileWriter fw = new FileWriter("./src/main/resources/log.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(city+" "+country+" "+temp+" "+wind+" "+hum+" "+dtf.format(now));
+            bw.newLine();
+
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public MeteoController() {
+        locations = new HashMap<>();
     }
 
 }
--- src/main/java/ro/mta/se/lab/model/MeteoModel.java
+++ src/main/java/ro/mta/se/lab/model/MeteoModel.java
@@ -1,4 +1,74 @@
 package ro.mta.se.lab.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Map;
+
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class MeteoModel {
+
+    private String temperature;
+    private String wind;
+    private String humidity;
+
+    public MeteoModel(String city) {
+        String API_key = "d41a3b43ac8cf4ceaa48bf6d6c1f78f4";
+        String API_url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+API_key;
+
+        try{
+            URL url = new URL(API_url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            StringBuffer content = new StringBuffer();
+
+            while ((line = br.readLine()) != null) {
+                content.append(line);
+            }
+            br.close();
+            conn.disconnect();
+
+            JSONParser parser = new JSONParser();
+
+            Map respMap = (Map) parser.parse(content.toString()); //jsonToMap (content.toString());
+            Map mainMap = (Map) parser.parse(respMap.get("main").toString());
+            Map windMap = (Map) parser.parse(respMap.get("wind").toString());
+
+            this.temperature = mainMap.get("temp").toString();
+            this.humidity = mainMap.get("humidity").toString();
+            this.wind = windMap.get("speed").toString();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getTemperature(){
+        return temperature;
+    }
+
+    public String getWind(){
+        return wind;
+    }
+
+    public String getHumidity(){
+        return humidity;
+    }
 }
